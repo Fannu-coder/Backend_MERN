7bab737eecb2d8b9f144b1798b53440571279883
Fannu-coder <codefannu@gmail.com>
2025-10-23 14:52:57 +0500

    Backend_Practice_Set#3

--- DAY_14_Tailwind_CSS/routes/hostRouter.js
+++ DAY_14_Tailwind_CSS/routes/hostRouter.js
@@ -3,13 +3,15 @@ const path= require('path');
 const express= require('express');
 const hostRouter= express.Router();
 
-const rootDir= require('../utils/pathUtil');
+const rootDir = require('../utils/pathUtil');
 
-hostRouter.get('/add-home',(req,res,next)=>{
+hostRouter.get("/add-home",(req,res,next)=>{
   res.sendFile(path.join(rootDir,'views','AddHome.html'));
 });
-hostRouter.post('/add-home',(req,res,next)=>{
+
+hostRouter.post("/add-home",(req,res,next)=>{
   console.log(req.body);
   res.sendFile(path.join(rootDir,'views','homeAdded.html'));
 });
-module.exports= hostRouter;
+
+module.exports = hostRouter;
--- DAY_14_Tailwind_CSS/routes/userRouter.js
+++ DAY_14_Tailwind_CSS/routes/userRouter.js
@@ -1,11 +1,12 @@
 const path= require('path');
 
 const express= require('express');
-const userRouter= express.Router();
+const userRouter = express.Router();
 
-const rootDir= require('../utils/pathUtil');
+const rootDir = require('../utils/pathUtil');
 
-userRouter.get('/',(req,res,next)=>{
+userRouter.get("/",(req,res,next)=>{
   res.sendFile(path.join(rootDir,'views','home.html'));
 });
-module.exports= userRouter;
+
+module.exports = userRouter;
